07d336f83f02af840bc187a1a425aea87f781eee
Ilya Gellert <gellertilya@gmail.com>
2025-09-09 23:42:09 +0300

    Create logger

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,1 +1,27 @@
 package logger
+
+import (
+	"github.com/ilindan-dev/shortener/internal/config"
+	"github.com/rs/zerolog"
+	"os"
+)
+
+// NewLogger creates a new configured instance of zerolog.Logger.
+func NewLogger(cfg *config.Config) (*zerolog.Logger, error) {
+	level, err := zerolog.ParseLevel(cfg.Logger.Level)
+	if err != nil {
+		// Default to info level if config is invalid or missing
+		level = zerolog.InfoLevel
+	}
+
+	consoleWriter := zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: "2006-01-02T15:04:05Z07:00"}
+
+	logger := zerolog.New(consoleWriter).With().
+		Timestamp().
+		Str("service", "delayed-notifier").
+		Caller().
+		Logger().
+		Level(level)
+
+	return &logger, nil
+}
